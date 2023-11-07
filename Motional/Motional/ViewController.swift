4daa4c20dd2437b52058bb56055a2675016f554c
ryan <concepting@protonmail.com>
2023-11-07 17:58:42 -0500

    Created XCode File

---
+++ Motional/Motional/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Motional
+//
+//  Created by Ryan on 11/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
