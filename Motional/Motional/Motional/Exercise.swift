7d3d66b2fb9b6a41357ed996ce1c8962f87832a5
ryan <concepting@protonmail.com>
2023-11-18 16:37:17 -0500

    wip

---
+++ Motional/Motional/Motional/Exercise.swift
@@ -0,0 +1,18 @@
+//
+//  Exercise.swift
+//  Motional
+//
+//  Created by Ryan on 11/18/23.
+//
+
+// Exercise.swift
+import Foundation
+
+struct Exercise: Codable {
+    let name: String
+    let type: String
+    let muscle: String
+    let equipment: String
+    let difficulty: String
+    let instructions: String
+}
