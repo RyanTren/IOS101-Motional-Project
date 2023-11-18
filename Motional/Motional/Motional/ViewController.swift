d42ccfe237a4ed9354c5c6d2a1d2d2b9f6469707
ryan <concepting@protonmail.com>
2023-11-18 16:39:33 -0500

    a

--- Motional/Motional/Motional/ViewController.swift
+++ Motional/Motional/Motional/ViewController.swift
@@ -1,64 +1,19 @@
+//
+//  ViewController.swift
+//  Motional
+//
+//  Created by Ryan on 11/18/23.
+//
+
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
-    @IBOutlet weak var tableView: UITableView!
-
-    var exercises: [Exercise] = []
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.dataSource = self
-        tableView.delegate = self
-        fetchExercises()
-    }
-
-    func fetchExercises() {
-        guard let url = URL(string: "https://api.api-ninjas.com/v1/exercises?muscle=") else {
-            return
-        }
-
-        let task = URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
-            guard let data = data, error == nil else {
-                print("Error fetching data: \(error?.localizedDescription ?? "Unknown error")")
-                return
-            }
-
-            do {
-                let decoder = JSONDecoder()
-                let fetchedExercises = try decoder.decode([Exercise].self, from: data)
-                
-                // Update exercises on the main thread
-                DispatchQueue.main.async {
-                    self?.exercises = fetchedExercises
-                    self?.tableView.reloadData()
-                }
-            } catch {
-                print("Error decoding JSON: \(error.localizedDescription)")
-            }
-        }
-
-        task.resume()
+        // Do any additional setup after loading the view.
     }
 
 
-    // MARK: - UITableViewDataSource Methods
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return exercises.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ExerciseCell", for: indexPath) as! ExerciseCell
-        _ = exercises[indexPath.row]
-        //cell.configure(with: exercise)
-        return cell
-    }
-
-    // MARK: - UITableViewDelegate Methods
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        // Handle row selection, e.g., navigate to a detail view
-    }
 }
+
