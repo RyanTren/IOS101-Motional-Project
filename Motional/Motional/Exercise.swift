6959aa0dbd5503be6c10e032df497434ae18d96e
ryan <concepting@protonmail.com>
2023-11-18 16:13:19 -0500

    reset

--- Motional/Motional/Exercise.swift
+++ Motional/Motional/Exercise.swift
@@ -1,20 +1,11 @@
-//
-//  Exercise.swift
-//  Motional
-//
-//  Created by Ryan on 11/18/23.
-//
-
+// Exercise.swift
 import Foundation
 
-struct ExerciseFeed: Decodable {
-    let results: [Exercise]
-}
-
-struct Exercise: Decodable {
-    let name, type, muscle, equipment, difficulty, instructions: String
-
-    enum CodingKeys: String, CodingKey {
-        case name, type, muscle, equipment, difficulty, instructions
-    }
+struct Exercise: Codable {
+    let name: String
+    let type: String
+    let muscle: String
+    let equipment: String
+    let difficulty: String
+    let instructions: String
 }
--- Motional/Motional/ViewController.swift
+++ Motional/Motional/ViewController.swift
@@ -1,86 +1,63 @@
+// ExerciseListViewController.swift
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        
-        // Set custom names for rows 0-2
-        switch indexPath.row {
-        case 0:
-            cell.textLabel?.text = "Push Day"
-        case 1:
-            cell.textLabel?.text = "Pull Day"
-        case 2:
-            cell.textLabel?.text = "Leg Day"
-        default:
-            break
-        }
-        
-        cell.textLabel?.textAlignment = .center
-        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 30)
-        cell.textLabel?.textColor = UIColor.black
-        
-        return cell
-    }
-    
+class ExerciseListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
     @IBOutlet weak var tableView: UITableView!
-    
+
+    var exercises: [Exercise] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         tableView.dataSource = self
         tableView.delegate = self
+        fetchExercises()
     }
-    
+
+    func fetchExercises() {
+        guard let url = URL(string: "https://api.api-ninjas.com/v1/exercises?muscle=") else {
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
+            guard let data = data, error == nil else {
+                print("Error fetching data: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+
+            do {
+                let decoder = JSONDecoder()
+                let fetchedExercises = try decoder.decode([Exercise].self, from: data)
+                self?.exercises = fetchedExercises
+
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            } catch {
+                print("Error decoding JSON: \(error.localizedDescription)")
+            }
+        }
+
+        task.resume()
+    }
+
+    // MARK: - UITableViewDataSource Methods
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return exercises.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ExerciseCell", for: indexPath) as! ExerciseTableViewCell
+        let exercise = exercises[indexPath.row]
+        cell.configure(with: exercise)
+        return cell
+    }
+
+    // MARK: - UITableViewDelegate Methods
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        
-        // Example: Navigate to a new view controller based on the selected row
-        switch indexPath.row {
-        case 0, 1, 2:
-            let detailViewController = DetailViewController()
-            detailViewController.modalTransitionStyle = .flipHorizontal // Set the desired transition style
-            navigationController?.pushViewController(detailViewController, animated: true)
-        default:
-            break
-        }
-    }
-    
-    private func fetchExercise(){
-        let url = URL(string: "https://api.api-ninjas.com/v1/exercises?muscle=")!
-        
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData)
-        
-        let session = URLSession.shared.dataTask(with: request){ data, response, error in
-            
-            if let error = error {
-                print("🚨 Request failed: \(error.localizedDescription)")
-                return
-            }
-            
-            
-            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
-                print("🚨 Server Error: response: \(String(describing: response))")
-                return
-            }
-            
-            
-            guard let data = data else {
-                print("🚨 No data returned from request")
-                return
-            }
-            
-            do{
-                let decode = JSONDecoder()
-                let dateFormatter = DateFormatter()
-                
-            }
-        }
-        
-        session.resume()
+        // Handle row selection, e.g., navigate to a detail view
     }
 }
