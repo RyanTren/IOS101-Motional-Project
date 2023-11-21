07613b4a39a881c0d6aa1dfc823b5c495857b540
ryan <concepting@protonmail.com>
2023-11-20 20:15:22 -0500

    Added Launch Screen and App Icon

---
+++ Motional/Motional/Motional/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Motional
+//
+//  Created by Ryan on 11/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Motional/Motional/Motional/gifViewController.swift
@@ -0,0 +1,18 @@
+//
+//  gifViewController.swift
+//  Motional
+//
+//  Created by Ryan on 11/20/23.
+//
+
+import Foundation
+import UIKit
+
+class gifViewController: UIViewController{
+    
+    
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+}
